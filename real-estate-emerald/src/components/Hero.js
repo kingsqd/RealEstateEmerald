4f31150343dfcabbf5a7e16c209c27b50e9e67c8
Hakeem King <kingsqd1@gmail.com>
2020-03-09 00:00:19 -0700

    title

---
+++ real-estate-emerald/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Hero extends Component {
+    render() {
+        return (
+            <div className="hero hero-lg mb-3">
+              <div className="hero-body text-center text-light">
+                <h1>Emerald Real Estate</h1>
+                <p className="mb-0">Bringing premium property right to your fingertips</p>
+              </div>
+            </div>
+          );
+    }
+}
+
+export default Hero
--- real-estate-emerald/src/index.js
+++ real-estate-emerald/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Hero from './components/Hero'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Home extends Component {
+    render() {
+        return (
+            <Hero />
+        );
+    }
+}
+
+ReactDOM.render(<Home />, document.getElementById('root'));
